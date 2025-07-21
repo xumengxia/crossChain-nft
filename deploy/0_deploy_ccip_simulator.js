2a255bda1fde0a9505612705603a70f688365c47
xumengxia <1540926580@qq.com>
2025-07-21 15:20:24 +0800

    删除多余打印

--- deploy/0_deploy_ccip_simulator.js
+++ deploy/0_deploy_ccip_simulator.js
@@ -1,10 +1,7 @@
 const { getNamedAccounts, deployments, network } = require("hardhat");
 module.exports = async ({ getNamedAccounts, deployments }) => {
-  console.log(process.env.PRIVATE_KEY, 'process.env.PRIVATE_KEY');
-
   const { firstAccount } = await getNamedAccounts();
   const { deploy, log } = deployments;
-  console.log("firstAccount", firstAccount);
 
   log("Deploying CCIP Simulator contract");
   await deploy("CCIPLocalSimulator", {
